7819ef73fb24a3cc1caa83b1f6d39ee10a1ddaa0
dlocc <dlocc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-20 07:15:36 +0000

    * Updated: Separated Twitter Bootstrap Dropdown from select for better control and Bootstrap compatibility * NEW: Toggle on/off Twitter Bootstrap dropdowns
    git-svn-id: https://plugins.svn.wordpress.org/open-table-widget/tags/1.1.1@775855 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/views/about.php
@@ -0,0 +1,41 @@
+<div class="open-table-about-wrap">
+    <p class="basic-label label">Basic Version <?php echo $this->version; ?></p>
+</div>
+<div class="metabox-holder clearfix">
+
+    <div class="sunrise-plugin-onehalf">
+        <div class="postbox">
+            <h3 class="hndle"><?php _e('Welcome to Open Table Widget for WordPress', $this->textdomain); ?></h3>
+
+            <div class="inside">
+
+                <p><?php _e('Open Table Widget makes it a breeze to integrate powerful restaurant reservations forms right into your WordPress website. Great for restaurants, nightclubs, bars and more!', $this->textdomain); ?></p>
+
+                <?php include('social-media.php'); ?>
+
+            </div>
+        </div>
+    </div>
+    <div class="sunrise-plugin-onehalf">
+        <div class="postbox">
+
+            <h3><?php _e('Upgrade to Open Table Widget Pro', $this->textdomain); ?></h3>
+
+            <div class="inside">
+                <p>Unlock powerful features and customization abilities by purchasing Open Table Widget Pro.</p>
+                <h4><em>Pro Version Release Coming Soon!</em></h4>
+                <ol>
+                    <li>Multi-Language Support</li>
+                    <li>Multi-Country Support</li>
+                    <li>Mobile Optimized</li>
+                    <li>Additional Datepickers</li>
+                    <li>1 year Priority Support</li>
+                    <li>Modal/Lightbox Support</li>
+                    <li>Powerful Shortcode</li>
+                </ol>
+            </div>
+        </div>
+        <!--.sunrise-plugin-onehalf -->
+    </div>
+    <!--.sunrise-plugin-onehalf -->
+</div><!-- .metabox-holder-->
